05fc29dcb0bacc0a44b20fa85af364a887216570
syedsaroshfarrukhdot <81307212+syedsaroshfarrukhdot@users.noreply.github.com>
2021-04-09 18:08:13 +0500

    Cors Olicy Fixed

--- app.js
+++ app.js
@@ -1,55 +1,53 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var cors = require("cors");
 
-var logger = require('morgan');
-var mongoose = require('mongoose')
-var config = require('config')
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var config = require("config");
 
-var indexRouter = require('./routes/index');
-var apiUserRouter = require('./routes/api/users');
-var apiProjectRouter = require('./routes/api/project');
-var apiTasksRouter = require('./routes/api/tasks');
-var apiDesignationRouter = require('./routes/api/designation');
-
-
+var indexRouter = require("./routes/index");
+var apiUserRouter = require("./routes/api/users");
+var apiProjectRouter = require("./routes/api/project");
+var apiTasksRouter = require("./routes/api/tasks");
+var apiDesignationRouter = require("./routes/api/designation");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(logger('dev'));
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', apiUserRouter);
-app.use('/projects', apiProjectRouter);
-app.use('/tasks', apiTasksRouter);
-app.use('/designation', apiDesignationRouter);
-
+app.use("/", indexRouter);
+app.use("/users", apiUserRouter);
+app.use("/projects", apiProjectRouter);
+app.use("/tasks", apiTasksRouter);
+app.use("/designation", apiDesignationRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
-
 
 mongoose
   .connect(config.get("db"), {
