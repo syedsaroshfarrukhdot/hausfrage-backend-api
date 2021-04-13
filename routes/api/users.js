774a811a3fb19efe8b1f6834e258f2c6ed17c371
syedsaroshfarrukhdot <81307212+syedsaroshfarrukhdot@users.noreply.github.com>
2021-04-13 22:05:12 +0500

    image upload fixed to only name

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,20 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const multer = require("multer")
+const multer = require("multer");
 var fs = require("fs");
 var path = require("path");
 const { User } = require("../../model/user");
-const bcrypt = require('bcryptjs');
-const _ = require('lodash')
-const jwt = require("jsonwebtoken")
-const config = require("config")
-const auth = require("../../middlewares/auth")
-const admin = require("../../middlewares/admin")
-
-
+const bcrypt = require("bcryptjs");
+const _ = require("lodash");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const auth = require("../../middlewares/auth");
+const admin = require("../../middlewares/admin");
 
 var Storage = multer.diskStorage({
-  destination: "uploads/",
+  destination: "public/uploads/",
   filename: (req, file, cb) => {
     cb(
       null,
@@ -37,33 +35,33 @@ var upload = multer({
   },
 }).single("image");
 
-
-
-
 /* GET Users */
-router.get('/',async function(req, res, next) {
-    let page = Number(req.query.page ? req.query.page : 1);
-    let perPage = Number(req.query.perPage ? req.query.perPage : 10);
-    let skipRecords = perPage * (page - 1);
-    let user = await User.find().skip(skipRecords).limit(perPage);
-    return res.send(user);
+router.get("/", async function (req, res, next) {
+  let page = Number(req.query.page ? req.query.page : 1);
+  let perPage = Number(req.query.perPage ? req.query.perPage : 10);
+  let skipRecords = perPage * (page - 1);
+  let user = await User.find().skip(skipRecords).limit(perPage);
+  return res.send(user);
 });
 
 /* Signup . */
-router.post("/register",upload, async (req, res) => {
-    let user = await User.findOne({email:req.body.email});
-    if (user) return res.status(400).send("User With Given Email Already Exsists");
-    user = new User();
-    user.firstName = req.body.firstName;
-    user.middleName = req.body.middleName;
-    user.lastName = req.body.lastName;
-    user.email = req.body.email;
-    user.image = req.file.path;
-    user.password = req.body.password;
-    await user.generateHashedPassword();
-    await user.save();
-    return res.send(_.pick(user,['firstName','middleName','lastName','email']));
-  });
+router.post("/register", upload, async (req, res) => {
+  let user = await User.findOne({ email: req.body.email });
+  if (user)
+    return res.status(400).send("User With Given Email Already Exsists");
+  user = new User();
+  user.firstName = req.body.firstName;
+  user.middleName = req.body.middleName;
+  user.lastName = req.body.lastName;
+  user.email = req.body.email;
+  user.image = req.file.filename;
+  user.password = req.body.password;
+  await user.generateHashedPassword();
+  await user.save();
+  return res.send(
+    _.pick(user, ["firstName", "middleName", "lastName", "email"])
+  );
+});
 
 // Sign In
 router.post("/login", async (req, res) => {
@@ -72,53 +70,67 @@ router.post("/login", async (req, res) => {
   let isValid = await bcrypt.compare(req.body.password, user.password);
   if (!isValid) return res.status(401).send("Invalid Password");
   let token = jwt.sign(
-    { _id: user._id, firstName: user.firstName, middleName: user.middleName, lastName : user.lastName , role: user.role },
+    {
+      _id: user._id,
+      firstName: user.firstName,
+      middleName: user.middleName,
+      lastName: user.lastName,
+      role: user.role,
+      image: user.image,
+    },
     config.get("jwtPrivateKey")
   );
   return res.send(token);
 });
 
-  // Update User
-  router.put("/:id", auth ,admin, upload ,async (req, res) => {
-    try {
-      let user = await User.findById(req.params.id);
-      if (!user) return res.status(400).send("User with given id is not present");
-      user.firstName = req.body.firstName;
-      user.middleName = req.body.middleName;
-      user.lastName = req.body.lastName;
-      user.email = req.body.email;
-      user.image = req.file.path;
-      user.password = req.body.password;
-      await user.generateHashedPassword();
-      await user.save();
-      let token = jwt.sign(
-        { _id: user._id, firstName: user.firstName, middleName: user.middleName, lastName : user.lastName , role: user.role },
-        config.get("jwtPrivateKey")
-      );
-      let dataToReturn = {
+// Update User
+router.put("/:id", auth, admin, upload, async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id);
+    if (!user) return res.status(400).send("User with given id is not present");
+    user.firstName = req.body.firstName;
+    user.middleName = req.body.middleName;
+    user.lastName = req.body.lastName;
+    user.email = req.body.email;
+    user.image = req.file.path;
+    user.password = req.body.password;
+    await user.generateHashedPassword();
+    await user.save();
+    let token = jwt.sign(
+      {
+        _id: user._id,
         firstName: user.firstName,
         middleName: user.middleName,
         lastName: user.lastName,
-        email: user.email,
-        token: user.token,
-      };
-      return res.send(dataToReturn);
-    } catch (err){
-      return res.status(400).send("Invalid Id"); // when id is inavlid
-    }
-  });
+        role: user.role,
+        image: user.image,
+      },
+      config.get("jwtPrivateKey")
+    );
+    let dataToReturn = {
+      firstName: user.firstName,
+      middleName: user.middleName,
+      lastName: user.lastName,
+      email: user.email,
+      token: user.token,
+    };
+    return res.send(dataToReturn);
+  } catch (err) {
+    return res.status(400).send("Invalid Id"); // when id is inavlid
+  }
+});
 
-  // Delete user
-  router.delete("/:id",auth,admin,async (req,res) => {
-    try {
-        let user = await User.findByIdAndDelete(req.params.id);
-        if (!user) {
-          return res.status(400).send("User with given id is not present"); // when there is no id in db
-        }
-        return res.send(user); // when everything is okay
-      } catch {
-        return res.status(400).send("Invalid Id"); // when id is inavlid
-      }
-  })
+// Delete user
+router.delete("/:id", auth, admin, async (req, res) => {
+  try {
+    let user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(400).send("User with given id is not present"); // when there is no id in db
+    }
+    return res.send(user); // when everything is okay
+  } catch {
+    return res.status(400).send("Invalid Id"); // when id is inavlid
+  }
+});
 
 module.exports = router;
